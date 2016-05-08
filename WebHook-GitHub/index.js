ca4b68c6098d5aeec215fe5ef15de70c0e4ba19c
Lucas Sargent <JJ11teen@hotmail.com>
2016-05-08 22:25:53 +0930

    testing

---
+++ WebHook-GitHub/index.js
@@ -0,0 +1,7 @@
+﻿module.exports = function (context, data) {
+    context.log('GitHub WebHook triggered!', data.comment.body);
+    context.res = {
+        body: 'New GitHub comment: ' + data.comment.body
+    };
+    context.done();
+}
